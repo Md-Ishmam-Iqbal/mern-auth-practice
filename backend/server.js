ac261bf89bbcc023e6f9118ee68edf009cff217b
Md Ishmam Iqbal <iqbal.ishmam@gmail.com>
2023-08-20 00:37:11 +0600

    created all api routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 8000;
 import userRoutes from "./routes/userRoutes.js";
 
@@ -11,5 +12,8 @@ app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server running on port ${port}`));
